d4845a861317ac7bbccdcb70fa52fff22238d012
vijay <vijaysalian94@gmail.com>
2021-10-23 13:37:34 +0530

    new final commits

---
+++ src/main/java/com/moneycontrol/page/OversoldStocksPage.java
@@ -0,0 +1,62 @@
+package com.moneycontrol.page;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class OversoldStocksPage {
+
+	public WebDriver driver;
+
+	private By oversoldTitle = By.xpath("//body[1]/div[2]/div[1]/div[1]/div[1]/h3[1]");
+	private By selectFilter = By.xpath("//select[@id='JS_diviGroupSelector']");
+	private By largeCap = By.xpath("//option[contains(text(),'Large Caps')]");
+	private By modalButton = By.xpath("//button[@class='close']");
+	private By filter = By.xpath("//span[@class='select2-selection__rendered']");
+	private By inputCapValues =By.xpath("//input[@class='select2-search__field']");
+	private By largeCapHeader = By.xpath("//div[@class='bbdr m-b-1']/h3");
+	private By tableHeader =By.xpath("//table[@id='DataTables_Table_0']/thead/tr/th");
+	private By tableRowSize = By.xpath("//table[@id='DataTables_Table_0']/tbody/tr");
+
+	public OversoldStocksPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public WebElement pageTitle() {
+		return driver.findElement(oversoldTitle);
+	}
+
+	public WebElement selectFilter() {
+		return driver.findElement(selectFilter);
+	}
+
+	public WebElement selectLargeCap() {
+		return driver.findElement(largeCap);
+	}
+
+	public WebElement modalCloseButton() {
+		return driver.findElement(modalButton);
+	}
+	
+	public WebElement selectFilterBox() {
+		return driver.findElement(filter);
+	}
+	
+	public WebElement inputFilterValue() {
+		return driver.findElement(inputCapValues);
+	}
+	
+	public WebElement getLargeCapHeader() {
+		return driver.findElement(largeCapHeader);
+	}
+	
+	public List<WebElement> tableHead() {
+		return driver.findElements(tableHeader);
+	}
+	
+	public List<WebElement> getTableRowSize() {
+		return driver.findElements(tableRowSize);
+	}
+}
---
+++ src/test/java/com/trendlyn/test/OversoldStocks.java
@@ -0,0 +1,101 @@
+package com.trendlyn.test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.awaitility.Awaitility;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.moneycontrol.page.OversoldStocksPage;
+import com.moneycontrol.utils.Baseinitialize;
+import com.moneycontrol.utils.CommonActions;
+import com.moneycontrol.utils.Logcheck;
+import com.moneycontrol.utils.moneyControlExcel;
+
+public class OversoldStocks extends Baseinitialize {
+
+	public static OversoldStocksPage oversold;
+	public static CommonActions commonActions;
+	public static Logger log = (Logger) LogManager.getLogger(Logcheck.class.getName());
+	public static moneyControlExcel mexcel;
+
+	@BeforeTest
+	public static void initialize() {
+
+		try {
+
+			driver = Baseinitialize.Initializedriver("Url3");
+			oversold = new OversoldStocksPage(driver);
+			commonActions = new CommonActions(driver);
+			mexcel = new moneyControlExcel(driver);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		log.info("This is info message after Initialize method");
+	}
+
+	@Test(priority = 0)
+	public static void getLargeCapOversoldStocks() {
+		try {
+
+			WebDriverWait wait = new WebDriverWait(driver, 30); // you can reuse this one
+
+			WebElement modalButton = oversold.modalCloseButton();
+			wait.until(ExpectedConditions.visibilityOf(modalButton));
+			modalButton.click();
+
+			Assert.assertEquals(oversold.pageTitle().isDisplayed(), true);
+
+			oversold.selectFilterBox().click();
+			oversold.inputFilterValue().sendKeys("Large Cap");
+			oversold.inputFilterValue().sendKeys(Keys.ENTER);
+
+			Assert.assertEquals("Oversold by Relative Strength Index (RSI) in S&P BSE Largecap",oversold.getLargeCapHeader().getText().trim().toString());
+			
+			wait.until(ExpectedConditions.visibilityOfAllElements(oversold.tableHead()));
+			
+			int i = 0;
+			for (WebElement header : oversold.tableHead()) {
+				System.out.println("header is :" + header.getText());
+				mexcel.createExcel("Oversold Stocks", 0, i, header.getText());
+				i++;
+				log.info("Message: Header of the body is added to the excel sheet");
+			}
+			
+
+			////table[@id='DataTables_Table_0']/tbody/tr[2]/td
+			int rowSize = oversold.getTableRowSize().size();
+					
+			String firstPart = "//table[@id='DataTables_Table_0']/tbody/tr[";
+			String secondPart="]/td";
+			for(int a=1;a<=rowSize;) {
+				int colSize = 0;
+				String finalPart = firstPart+a+secondPart;
+				for(WebElement row : driver.findElements(By.xpath(finalPart))) {
+					String rowDetails = row.getText();
+					System.out.println("The oversold stocks are : " + rowDetails);
+					mexcel.createExcel("Oversold Stocks", a, colSize, rowDetails);
+					colSize++;
+				}a++;
+			log.info("Message: Oversold stocks are added to the excel sheet");
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
